7933c02beeb2da36ac0a080aaf2884a8d22e8415
Apavayan Sinha <info@apavayan.com>
2015-11-08 17:59:03 +0530

    s3ve3gds: Update the LP ril blobs
    Signed-off-by: Apavayan Sinha <info@apavayan.com>

--- s3ve3gds-vendor-blobs.mk
+++ s3ve3gds-vendor-blobs.mk
@@ -17,20 +17,23 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/proprietary/blobs/s3ve3gds/vendor/lib/libdiag.so:system/blobs/s3ve3gds/vendor/lib/libdiag.so \
     $(LOCAL_PATH)/proprietary/blobs/s3ve3gds/vendor/lib/libdsutils.so:system/blobs/s3ve3gds/vendor/lib/libdsutils.so \
     $(LOCAL_PATH)/proprietary/blobs/s3ve3gds/vendor/lib/libdsucsd.so:system/blobs/s3ve3gds/vendor/lib/libdsucsd.so \
+    $(LOCAL_PATH)/proprietary/blobs/s3ve3gds/vendor/lib/libdsnetutils.so:system/blobs/s3ve3gds/vendor/lib/libdsnetutils.so \
     $(LOCAL_PATH)/proprietary/blobs/s3ve3gds/vendor/lib/libidl.so:system/blobs/s3ve3gds/vendor/lib/libidl.so \
     $(LOCAL_PATH)/proprietary/blobs/s3ve3gds/vendor/lib/libqcci_legacy.so:system/blobs/s3ve3gds/vendor/lib/libqcci_legacy.so \
     $(LOCAL_PATH)/proprietary/blobs/s3ve3gds/vendor/lib/libqmi.so:system/blobs/s3ve3gds/vendor/lib/libqmi.so \
     $(LOCAL_PATH)/proprietary/blobs/s3ve3gds/vendor/lib/libqmi_cci.so:system/blobs/s3ve3gds/vendor/lib/libqmi_cci.so \
     $(LOCAL_PATH)/proprietary/blobs/s3ve3gds/vendor/lib/libqmi_client_qmux.so:system/blobs/s3ve3gds/vendor/lib/libqmi_client_qmux.so \
     $(LOCAL_PATH)/proprietary/blobs/s3ve3gds/vendor/lib/libqmi_csi.so:system/blobs/s3ve3gds/vendor/lib/libqmi_csi.so \
-    $(LOCAL_PATH)/proprietary/blobs/s3ve3gds/vendor/lib/libqmiservices.so:system/blobs/s3ve3gds/vendor/lib/libqmiservices.so
+    $(LOCAL_PATH)/proprietary/blobs/s3ve3gds/vendor/lib/libqmiservices.so:system/blobs/s3ve3gds/vendor/lib/libqmiservices.so \
+    $(LOCAL_PATH)/proprietary/blobs/s3ve3gds/vendor/lib/libqmi_common_so.so:system/blobs/s3ve3gds/vendor/lib/libqmi_common_so.so \
+    $(LOCAL_PATH)/proprietary/blobs/s3ve3gds/vendor/lib/libqmi_encdec.so:system/blobs/s3ve3gds/vendor/lib/libqmi_encdec.so
 
 # Radio
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/proprietary/blobs/s3ve3gds/bin/ds_fmc_appd:system/blobs/s3ve3gds/bin/ds_fmc_appd \
     $(LOCAL_PATH)/proprietary/blobs/s3ve3gds/bin/qmuxd:system/blobs/s3ve3gds/bin/qmuxd \
     $(LOCAL_PATH)/proprietary/blobs/s3ve3gds/bin/radish:system/blobs/s3ve3gds/bin/radish \
     $(LOCAL_PATH)/proprietary/blobs/s3ve3gds/bin/rild:system/blobs/s3ve3gds/bin/rild \
+    $(LOCAL_PATH)/proprietary/blobs/s3ve3gds/bin/rmt_storage:system/blobs/s3ve3gds/bin/rmt_storage \
     $(LOCAL_PATH)/proprietary/blobs/s3ve3gds/lib/libatparser.so:system/blobs/s3ve3gds/lib/libatparser.so \
     $(LOCAL_PATH)/proprietary/blobs/s3ve3gds/lib/libcordon.so:system/blobs/s3ve3gds/lib/libcordon.so \
     $(LOCAL_PATH)/proprietary/blobs/s3ve3gds/lib/libfactoryutil.so:system/blobs/s3ve3gds/lib/libfactoryutil.so \
@@ -41,6 +44,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/proprietary/blobs/s3ve3gds/lib/libsec-ril.so:system/blobs/s3ve3gds/lib/libsec-ril.so \
     $(LOCAL_PATH)/proprietary/blobs/s3ve3gds/lib/libsec-ril-dsds.so:system/blobs/s3ve3gds/lib/libsec-ril-dsds.so \
     $(LOCAL_PATH)/proprietary/blobs/s3ve3gds/lib/libril.so:system/blobs/s3ve3gds/lib/libril.so \
+    $(LOCAL_PATH)/proprietary/blobs/s3ve3gds/lib/librilutils.so:system/blobs/s3ve3gds/lib/librilutils.so \
     $(LOCAL_PATH)/proprietary/blobs/s3ve3gds/vendor/lib/libconfigdb.so:system/blobs/s3ve3gds/vendor/lib/libconfigdb.so \
     $(LOCAL_PATH)/proprietary/blobs/s3ve3gds/vendor/lib/libril-qcril-hook-oem.so:system/blobs/s3ve3gds/vendor/lib/libril-qcril-hook-oem.so \
     $(LOCAL_PATH)/proprietary/blobs/s3ve3gds/vendor/lib/libxml.so:system/blobs/s3ve3gds/vendor/lib/libxml.so
